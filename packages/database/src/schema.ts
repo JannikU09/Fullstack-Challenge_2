5756582a79ad83db30977a877757623efd04f09b
JannikU09 <j.uphoff@innfactory.de>
2026-04-15 11:08:44 +0200

    feat: books-Tabelle im Schema erstellt, mit - id - title - isbn - year - authorId

--- packages/database/src/schema.ts
+++ packages/database/src/schema.ts
@@ -1,8 +1,16 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const authors = pgTable("Author", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
+});
+
+export const books = pgTable("Books", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  isbn: text("isbn").unique(),
+  year: integer("year"),
+  authorId: integer("authorId").references(() => authors.id).notNull(),
 });
 
 export type Author = typeof authors.$inferSelect;
