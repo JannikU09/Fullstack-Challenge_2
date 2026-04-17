76b23449be28bd7f6d0b5452b56976042429b001
JannikU09 <j.uphoff@innfactory.de>
2026-04-17 11:08:18 +0200

    Sortierung in der route.ts implementiert, sodass nach authorId und year geordnet wird.

--- packages/web/src/app/api/books/route.ts
+++ packages/web/src/app/api/books/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const allBooks = await db.select().from(books).innerJoin(authors, eq(books.authorId, authors.id));
+        const allBooks = await db.select().from(books).innerJoin(authors, eq(books.authorId, authors.id)).orderBy(books.authorId, books.year);
         return NextResponse.json(allBooks, { statusText: "Liste der Bücher" });
     } catch {
         return NextResponse.json({ error: "Failed to fetch books" }, { status: 500 });
