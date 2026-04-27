3c7c9e01d2a2364476c5a24e1704414ae2a19b55
JannikU09 <j.uphoff@innfactory.de>
2026-04-27 14:25:24 +0200

    Button-Komponente implementiert.

---
+++ packages/web/src/componentes/ui/Button.tsx
@@ -0,0 +1,27 @@
+
+interface ButtonProps {
+    children: React.ReactNode;
+    variant: "primary" | "danger";
+    type?: "button" | "submit";
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    disabled?: true | false;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+    children,
+    variant,
+    type,
+    onClick,
+    disabled,
+}) => {
+    return (
+        <button
+            className={`button ${variant}`}
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    )
+}
