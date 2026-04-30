c85e4ef95431bca64bd96a08643a8077520d62ad
JannikU09 <j.uphoff@innfactory.de>
2026-04-30 07:59:22 +0200

    style: Button-Komponente formatiert.

--- packages/web/src/componentes/ui/Button.tsx
+++ packages/web/src/componentes/ui/Button.tsx
@@ -1,27 +1,15 @@
-
 interface ButtonProps {
-    children: React.ReactNode;
-    variant: "primary" | "danger";
-    type?: "button" | "submit";
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
-    disabled?: true | false;
-};
-
-export const Button: React.FC<ButtonProps> = ({
-    children,
-    variant,
-    type,
-    onClick,
-    disabled,
-}) => {
-    return (
-        <button
-            className={`button ${variant}`}
-            type={type}
-            onClick={onClick}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    )
+  children: React.ReactNode;
+  variant: "primary" | "danger";
+  type?: "button" | "submit";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: true | false;
 }
+
+export const Button: React.FC<ButtonProps> = ({ children, variant, type, onClick, disabled }) => {
+  return (
+    <button className={`button ${variant}`} type={type} onClick={onClick} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
