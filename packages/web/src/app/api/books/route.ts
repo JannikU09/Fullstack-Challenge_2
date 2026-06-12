42842ec1ede2a504aae745bf699efdabdea2f134
JannikU09 <j.uphoff@innfactory.de>
2026-06-12 11:17:47 +0200

    Zu der GET-Funktion, Suche und Pagination hinzugefügt.

--- packages/web/src/app/api/books/route.ts
+++ packages/web/src/app/api/books/route.ts
@@ -1,5 +1,5 @@
 import { authors, books, db } from "@repo/database";
-import { eq, ilike, or } from "drizzle-orm";
+import { count, eq, ilike, or } from "drizzle-orm";
 import { NextResponse } from "next/server";
 import * as z from "zod";
 
@@ -16,6 +16,13 @@ export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const query = searchParams.get("q");
   const authorId = searchParams.get("authorId");
+  const page = Number(searchParams.get("page"));
+  const pageSize =
+    Number(searchParams.get("pageSize")) <= 100 === true
+      ? Number(searchParams.get("pageSize"))
+      : 100;
+  const offset = (page - 1) * pageSize;
+  console.log("page:", page, "pageSize:", pageSize, "offset:", offset);
   console.log(query, authorId);
   const parsedAuthorId = authorId ? Number(authorId) : null;
   try {
@@ -25,15 +32,35 @@ export async function GET(req: Request) {
         .from(books)
         .where(or(ilike(books.title, `%${query}%`), eq(books.authorId, parsedAuthorId)))
         .innerJoin(authors, eq(books.authorId, authors.id))
-        .orderBy(books.authorId, books.year);
-      return NextResponse.json(allBooks, { statusText: "Liste der gesuchten Bücher" });
+        .orderBy(books.authorId, books.year)
+        .limit(pageSize)
+        .offset(offset);
+
+      const totalBooks = await db
+        .select({ count: count() })
+        .from(books)
+        .where(or(ilike(books.title, `%${query}%`), eq(books.authorId, parsedAuthorId)));
+      console.log(totalBooks);
+      return NextResponse.json(
+        { data: allBooks, total: totalBooks[0].count ?? 0 },
+        { statusText: "Liste der gesuchten Bücher" },
+      );
     } else {
       const allBooks = await db
         .select()
         .from(books)
         .innerJoin(authors, eq(books.authorId, authors.id))
-        .orderBy(books.authorId, books.year);
-      return NextResponse.json(allBooks, { statusText: "Liste aller Bücher" });
+        .orderBy(books.authorId, books.year)
+        .limit(pageSize)
+        .offset(offset);
+
+      const totalBooks = await db.select({ count: count() }).from(books);
+      console.log(totalBooks);
+
+      return NextResponse.json(
+        { data: allBooks, total: totalBooks[0].count ?? 0 },
+        { statusText: "Liste aller Bücher" },
+      );
     }
   } catch {
     return NextResponse.json({ error: "Failed to fetch books" }, { status: 500 });
