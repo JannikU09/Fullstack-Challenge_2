15617b49912afd02000e771244cbc0d9ac504de3
JannikU09 <j.uphoff@innfactory.de>
2026-08-07 13:58:10 +0200

    Default duration für sonner toast zu 2.5 Sekunden geändert.

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Toaster
             richColors
             expand
+            duration={2500}
           />
           <main>{children}</main>
         </div>
