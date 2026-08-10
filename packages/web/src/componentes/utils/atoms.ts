dabbc4100a721ce9434a70393581ba1a3bf5eddc
JannikU09 <j.uphoff@innfactory.de>
2026-08-10 13:01:28 +0200

    feat: Datei für jotai-Atoms hinzugefügt.

---
+++ packages/web/src/componentes/utils/atoms.ts
@@ -0,0 +1,9 @@
+import { atom } from "jotai";
+import type { Author } from "../../app/interfaces/Author";
+import type { BookWithAuthor } from "../../app/interfaces/BookWithAuthor";
+
+export const allBooksAtom = atom<BookWithAuthor[]>([]);
+export const allAuthorsAtom = atom<Author[]>([]);
+export const pageAtom = atom("1");
+export const pageSizeAtom = atom("20");
+export const totalPagesAtom = atom(1);
