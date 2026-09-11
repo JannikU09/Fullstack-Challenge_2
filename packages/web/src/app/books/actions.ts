9bc7c4d1b2fae20bc4f32d703c58d897f659b392
JannikU09 <j.uphoff@innfactory.de>
2026-09-11 08:25:59 +0200

    Type BookInput geändert. parameter data zu bookInput geändert.

--- packages/web/src/app/books/actions.ts
+++ packages/web/src/app/books/actions.ts
@@ -1,55 +1,51 @@
 "use server";
 
-import { authors, books, db } from "@repo/database";
+import { books, db } from "@repo/database";
 import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
-import { NextResponse } from "next/server";
 import * as z from "zod";
 
-export const schema = z.object({
+const schema = z.object({
     title: z.string().min(1),
     authorId: z.number().int().positive(),
     isbn: z.string().min(13).max(13),
     year: z.number().int().min(1000).max(9999).optional(),
 });
 
-type BookInput = {
-    title: string;
-    authorId: number;
-    isbn: string;
-    year?: number;
-};
+type BookInput = z.infer<typeof schema>;
 
-export async function createBookAction(data: BookInput) {
-    const result = schema.safeParse(data, { reportInput: true });
+export async function createBookAction(bookInput: BookInput) {
+    const result = schema.safeParse(bookInput, { reportInput: true });
     console.log(result);
 
     if (!result.success) {
-        return NextResponse.json({ error: "Validation Error" }, { status: 400 });
+        return { success: false, error: "Validation Error" };
     }
 
     await db.insert(books).values(result.data).returning();
     revalidatePath("/books");
-};
+    return { success: true };
+}
 
-export async function updateBookAction(id: number, data: BookInput) {
-    const result = schema.safeParse(data, { reportInput: true });
+export async function updateBookAction(id: number, bookInput: BookInput) {
+    const result = schema.safeParse(bookInput, { reportInput: true });
     console.log(result);
 
     if (!result.success) {
-        return NextResponse.json({ error: "Validation Error" }, { status: 400 });
-    };
+        return { success: false, error: "Validation Error" };
+    }
 
     await db.update(books).set(result.data).where(eq(books.id, id)).returning();
     revalidatePath("/books");
-};
+    return { success: true };
+}
 
 export async function deleteBookAction(id: number) {
     try {
         await db.delete(books).where(eq(books.id, id)).returning();
         revalidatePath("/books");
-        return NextResponse.json({ message: "Erfolgreich gelöscht" }, { status: 200 });
-    } catch {
-        return NextResponse.json({ error: "Interner Serverfehler" }, { status: 500 });
+        return { success: true, message: "Erfolgreich gelöscht" };
+    } catch (error) {
+        return { success: false, error: error };
     }
-};
+}
