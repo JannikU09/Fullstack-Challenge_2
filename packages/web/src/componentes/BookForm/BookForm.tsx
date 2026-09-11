512928bf93d7dcf528bc9a5ea576186894c8d398
JannikU09 <j.uphoff@innfactory.de>
2026-09-11 08:01:21 +0200

    Import eingefügt.

--- packages/web/src/componentes/BookForm/BookForm.tsx
+++ packages/web/src/componentes/BookForm/BookForm.tsx
@@ -2,7 +2,9 @@
 
 import { Box, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { useState } from "react";
+import { createBookAction, deleteBookAction, updateBookAction } from "../../app/books/actions";
 import type { BookFormProps } from "../../app/interfaces/BookFormProps";
+import type { BookWithAuthor } from "../../app/interfaces/BookWithAuthor";
 import { Button } from "../ui/Button";
 import "./bookForm.css";
 
