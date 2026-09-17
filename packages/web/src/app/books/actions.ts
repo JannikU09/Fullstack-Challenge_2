396318d079173b2fba3abdf66482a92f5c703484
JannikU09 <j.uphoff@innfactory.de>
2026-09-17 09:42:15 +0200

    feature: Funktion getBooks implementiert, um die Bücher aus der Datenbank zu laden. Funktion getAuthors implementiert um die Autoren, aus der Datenbank zu laden.

--- packages/web/src/app/books/actions.ts
+++ packages/web/src/app/books/actions.ts
@@ -1,9 +1,12 @@
 "use server";
 
-import { books, db } from "@repo/database";
-import { eq } from "drizzle-orm";
+import { authors, books, db } from "@repo/database";
+import { and, count, eq, ilike } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import * as z from "zod";
+import type { Author } from "../interfaces/Author";
+import type { BookWithAuthor } from "../interfaces/BookWithAuthor";
+import type { BookSearchParams } from "../interfaces/searchParams";
 
 const schema = z.object({
     title: z.string().min(1),
@@ -12,9 +15,60 @@ const schema = z.object({
     year: z.number().int().min(1000).max(9999).optional(),
 });
 
-type BookInput = z.infer<typeof schema>;
+export async function getBooks(params: BookSearchParams) {
+    const offset: number = (params.page - 1) * params.pageSize;
+    const filters = [];
 
-export async function createBookAction(bookInput: BookInput) {
+    console.log(params.page, params.pageSize);
+
+    if (params.q) {
+        filters.push(ilike(books.title, `%${params.q}%`));
+        console.log(params.q);
+    }
+    if (params.authorId) {
+        filters.push(eq(books.authorId, Number(params.authorId)));
+        console.log(params.authorId);
+    }
+    if (params.page) {
+        console.log(params.page);
+    }
+    if (params.pageSize) {
+        console.log(params.pageSize);
+    }
+
+    console.log("filters", filters);
+
+    const isFilter = filters.length ? and(...filters) : undefined;
+
+    const [data, totalBooks] = await Promise.all([
+        db
+            .select()
+            .from(books)
+            .innerJoin(authors, eq(books.authorId, authors.id))
+            .where(isFilter)
+            .orderBy(books.authorId, books.year)
+            .limit(Number(params.pageSize))
+            .offset(offset),
+        db.select({ total: count() }).from(books).where(isFilter),
+    ]);
+
+    const total = totalBooks[0].total;
+    const totalPages = Math.ceil(total / params.pageSize);
+
+    return {
+        data,
+        total,
+        totalPages,
+        search: params.q ?? "",
+    };
+}
+
+export async function getAuthors() {
+    const allAuthors: Author[] = await db.select().from(authors);
+    return allAuthors;
+}
+
+export async function createBookAction(bookInput: BookWithAuthor) {
     const result = schema.safeParse(bookInput, { reportInput: true });
     console.log(result);
 
@@ -27,7 +81,7 @@ export async function createBookAction(bookInput: BookInput) {
     return { success: true };
 }
 
-export async function updateBookAction(id: number, bookInput: BookInput) {
+export async function updateBookAction(id: number, bookInput: BookWithAuthor) {
     const result = schema.safeParse(bookInput, { reportInput: true });
     console.log(result);
 
