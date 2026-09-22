617be8b7346288aeda5a0ba6a52b7982d7eb90c4
JannikU09 <j.uphoff@innfactory.de>
2026-09-22 14:29:21 +0200

    Noch nicht fertig - Suche implementiert.

--- packages/web/src/app/books/BooksFilters.tsx
+++ packages/web/src/app/books/BooksFilters.tsx
@@ -2,20 +2,32 @@
 
 import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { useAtom } from "jotai";
+import { useState } from "react";
+import { toast } from "sonner";
 import { Button } from "../../componentes/ui/Button";
 import { authorIdAtom, queryAtom } from "../../componentes/utils/atoms";
+import { useDebounce } from "../../lib/useDebounce";
 import type { Author } from "../interfaces/Author";
+import type { BookWithAuthor } from "../interfaces/BookWithAuthor";
 import type { BookSearchParams } from "../interfaces/searchParams";
 import { getAuthors, getBooks } from "./actions";
 
 type BooksFiltersProps = {
-    authors: Author[],
-    search: BookSearchParams,
-}
+    authors: Author[];
+    search: BookSearchParams;
+    total: number;
+    books: BookWithAuthor[];
+};
 
-export const BooksFilters = ({ authors, search }: BooksFiltersProps) => {
+export const BooksFilters = ({ authors, search, total, books }: BooksFiltersProps) => {
     const [query, setQuery] = useAtom(queryAtom);
     const [authorId, setAuthorId] = useAtom(authorIdAtom);
+    const [search2, setSearch2] = useState(search.q);
+    const [book, setBook] = useState(books);
+
+    function handleSearch() {
+        toast.info(`Anzahl der Bücher: ${total}`);
+    }
 
     return (
         <div className="search">
@@ -23,9 +35,13 @@ export const BooksFilters = ({ authors, search }: BooksFiltersProps) => {
                 style={{
                     width: "100%",
                 }}
-                value={query}
+                value={search2}
                 label={"Search"}
-                onChange={(event) => setQuery(event.target.value)}
+                onChange={async (event) => {
+                    setSearch2(event.target.value);
+                    const books = await getBooks({ ...search, q: event.target.value });
+                    setBook(books);
+                }}
                 autoComplete="off"
             />
             <div style={{ margin: "auto 5px" }} />
@@ -45,10 +61,10 @@ export const BooksFilters = ({ authors, search }: BooksFiltersProps) => {
             </FormControl>
 
             <div className="searchButton">
-                <Button variant="primary" type="button" onClick={() => getBooks}>
+                <Button variant="primary" type="button" onClick={() => handleSearch()}>
                     <strong>Search</strong>
                 </Button>
-                <Button variant="primary" type="button" onClick>
+                <Button variant="primary" type="button">
                     <strong>Reset</strong>
                 </Button>
             </div>
