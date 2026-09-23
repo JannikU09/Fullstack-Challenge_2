992979a97f1316ff99cd4b3676ca1b6247d0d298
JannikU09 <j.uphoff@innfactory.de>
2026-09-23 10:58:49 +0200

    refactor: Initiale Werte für page und pageSize geändert.

--- packages/web/src/componentes/utils/atoms.ts
+++ packages/web/src/componentes/utils/atoms.ts
@@ -4,8 +4,9 @@ import type { BookWithAuthor } from "../../app/interfaces/BookWithAuthor";
 
 export const allBooksAtom = atom<BookWithAuthor[]>([]);
 export const allAuthorsAtom = atom<Author[]>([]);
-export const pageAtom = atom("1");
-export const pageSizeAtom = atom("20");
+export const pageAtom = atom(1);
+export const pageSizeAtom = atom(20);
 export const totalPagesAtom = atom(1);
 export const queryAtom = atom("");
 export const authorIdAtom = atom("");
+export const pageSizeValueAtom = atom([5, 20, 50, 75, 100]);
