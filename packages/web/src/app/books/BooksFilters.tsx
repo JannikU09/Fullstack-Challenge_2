28c9b45c8fa846dd4d7b68a502756be44e8e2a51
JannikU09 <j.uphoff@innfactory.de>
2026-09-23 10:41:40 +0200

    refactor: Suche verbessert, mit useSearchParams von "next/navigation". Reset-Funktion implementiert.

--- packages/web/src/app/books/BooksFilters.tsx
+++ packages/web/src/app/books/BooksFilters.tsx
@@ -1,32 +1,53 @@
 "use client";
 
 import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
-import { useAtom } from "jotai";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
-import { toast } from "sonner";
 import { Button } from "../../componentes/ui/Button";
-import { authorIdAtom, queryAtom } from "../../componentes/utils/atoms";
-import { useDebounce } from "../../lib/useDebounce";
 import type { Author } from "../interfaces/Author";
-import type { BookWithAuthor } from "../interfaces/BookWithAuthor";
-import type { BookSearchParams } from "../interfaces/searchParams";
-import { getAuthors, getBooks } from "./actions";
 
 type BooksFiltersProps = {
     authors: Author[];
-    search: BookSearchParams;
-    total: number;
-    books: BookWithAuthor[];
 };
 
-export const BooksFilters = ({ authors, search, total, books }: BooksFiltersProps) => {
-    const [query, setQuery] = useAtom(queryAtom);
-    const [authorId, setAuthorId] = useAtom(authorIdAtom);
-    const [search2, setSearch2] = useState(search.q);
-    const [book, setBook] = useState(books);
+export const BooksFilters = ({ authors }: BooksFiltersProps) => {
+    const [q, setQ] = useState("");
+    const [authorId, setAuthorId] = useState("");
 
-    function handleSearch() {
-        toast.info(`Anzahl der Bücher: ${total}`);
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    function handleSearch(term: string) {
+        const params = new URLSearchParams(searchParams);
+        if (term) {
+            params.set("q", term);
+            setQ(term)
+        } else {
+            params.delete("q");
+            setQ("");
+        }
+        replace(`${pathname}?${params.toString()}`);
+    }
+
+    function handleAuhorIdChange(id: number) {
+        const params = new URLSearchParams(searchParams);
+        if (id) {
+            params.set("authorId", id);
+            setAuthorId(id);
+        } else {
+            params.delete("authorId");
+        }
+        replace(`${pathname}?${params.toString()}`);
+    }
+
+    function reset() {
+        const params = new URLSearchParams(searchParams);
+        params.delete("q");
+        params.delete("authorId");
+        setAuthorId("");
+        setQ("")
+        replace(`${pathname}?${params.toString()}`);
     }
 
     return (
@@ -35,22 +56,20 @@ export const BooksFilters = ({ authors, search, total, books }: BooksFiltersProp
                 style={{
                     width: "100%",
                 }}
-                value={search2}
                 label={"Search"}
-                onChange={async (event) => {
-                    setSearch2(event.target.value);
-                    const books = await getBooks({ ...search, q: event.target.value });
-                    setBook(books);
+                onChange={(event) => {
+                    handleSearch(event.target.value);
                 }}
                 autoComplete="off"
+                value={q}
             />
             <div style={{ margin: "auto 5px" }} />
             <FormControl fullWidth>
                 <InputLabel>Author</InputLabel>
                 <Select
-                    value={authorId}
-                    onChange={(event) => setAuthorId(event.target.value)}
+                    onChange={(event) => handleAuhorIdChange(event.target.value)}
                     label="Author"
+                    value={authorId}
                 >
                     {authors.map((author) => (
                         <MenuItem key={author.id} value={author.id}>
@@ -61,10 +80,7 @@ export const BooksFilters = ({ authors, search, total, books }: BooksFiltersProp
             </FormControl>
 
             <div className="searchButton">
-                <Button variant="primary" type="button" onClick={() => handleSearch()}>
-                    <strong>Search</strong>
-                </Button>
-                <Button variant="primary" type="button">
+                <Button variant="primary" type="button" onClick={() => reset()}>
                     <strong>Reset</strong>
                 </Button>
             </div>
