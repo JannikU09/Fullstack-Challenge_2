fc9670f4fce72bebafbcf78cfa0de91ea122751b
JannikU09 <j.uphoff@innfactory.de>
2026-09-25 12:07:43 +0200

    refactor: Page-Dropdown mit Pagination Component von "@/mui/material" ersetzt.

--- packages/web/src/app/books/BooksPagination.tsx
+++ packages/web/src/app/books/BooksPagination.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Pagination, Select } from "@mui/material";
 import { useAtomValue } from "jotai";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { pageSizeValueAtom } from "../../componentes/utils/atoms";
 import "./page.css";
 
@@ -19,57 +19,77 @@ export const BooksPagination = ({ totalPages }: BooksPaginationProps) => {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
+    const params = new URLSearchParams(searchParams);
+
+    useEffect(() => {
+        setPage(Number(params.get("page")));
+        setPageSize(Number(params.get("pageSize")))
+    }, [params.get]);
+
 
     let pages = [];
     pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
+
     function handlePageNumber(pageNumber: number) {
-        const params = new URLSearchParams(searchParams);
         if (pageNumber) {
-            params.set("page", pageNumber)
+            params.set("page", pageNumber);
             setPage(pageNumber);
         } else {
             params.set("page", 1);
+            setPage(1);
         }
         replace(`${pathname}?${params.toString()}`);
-    };
+    }
 
     function handlePageSize(pageSizeNow: number) {
-        const params = new URLSearchParams(searchParams);
         if (pageSizeNow) {
+            params.set("page", 1);
             params.set("pageSize", pageSizeNow);
-            params.set("page", 1)
             setPageSize(pageSizeNow);
             setPage(1);
         } else {
             params.set("pageSize", 20);
+            setPageSize(20);
         }
         replace(`${pathname}?${params.toString()}`);
-    };
+    }
 
     return (
-        <div className="pageSizeSelect">
-            <FormControl fullWidth disabled={totalPages <= 1}>
-                <InputLabel>Page</InputLabel>
-                <Select value={page} label="Page" onChange={(event) => handlePageNumber(event.target.value)}>
-                    {pages.map((currentPage) => (
-                        <MenuItem key={currentPage} value={currentPage}>
-                            {currentPage}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <div style={{ margin: "5px" }} />
-            <FormControl fullWidth>
-                <InputLabel>Page Size</InputLabel>
-                <Select value={pageSize} label="Page Size" onChange={(event) => handlePageSize(event.target.value)}>
-                    {pageSizeValue.map((size) => (
-                        <MenuItem key={size} value={size}>
-                            {size}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+        <div>
+            <div className="choosePage">
+                <Pagination
+                    count={pages.length}
+                    page={page}
+                    onChange={(_event, page) => handlePageNumber(page)}
+                    shape="rounded"
+                    sx={{
+                        "& .MuiPaginationItem-root": {
+                            width: "10px",
+                            height: "24px",
+                        },
+                    }}
+                    showFirstButton
+                    showLastButton
+                />
+            </div>
+            <div className="pageSizeSelect">
+                <FormControl sx={{ width: "100px" }}>
+                    <InputLabel>Page Size</InputLabel>
+                    <Select
+                        value={pageSize}
+                        label="Page Size"
+                        onChange={(event) => handlePageSize(event.target.value)}
+                    >
+                        {pageSizeValue.map((size) => (
+                            <MenuItem key={size} value={size}>
+                                {size}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <div style={{ margin: "5px" }} />
+            </div>
         </div>
     );
 };
